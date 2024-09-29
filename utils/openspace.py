a93b5ff15f6838fe1d43c0cc59ae45ae0ff66076
Rik <71097521+ricosaxo@users.noreply.github.com>
2024-09-29 12:18:07 +0200

    coding_openspace_29_09

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,7 +1,7 @@
 import csv
 import random as rd
 from file_utils import open_database
-from table import Table
+from table import Table,Seat
 
 
 class Openspace(Table):
@@ -23,9 +23,7 @@ class Openspace(Table):
     def __init__ (self, number_of_tables = 6):
         self.number_of_tables = number_of_tables
         self.tables = []
-        super().__init__()
-
-        pass
+            
 
     def organize(names :list):
         
@@ -41,15 +39,43 @@ class Openspace(Table):
 
         rd.shuffle(names)
 
+        # Assigning the students to the seats and populating a list with seat objects
+        list_of_seats = []
+        
+        for name in names:
+            seat = Seat()
+            seat.set_occupant(name)
+            list_of_seats.append(seat) 
 
+        # Initializing the table objects and populating the self.tables with table objects
+        
+        for _ in range(self.number_of_tables):
+            table = Table()
+            self.tables.append(table)
+        
+        # Start to assign the seat objects to the table objects
+        seat_index = 0
+        while seat_index < len(list_of_seats):
+            for table in self.tables:
+                if seat_index < len(list_of_seats) and table.has_free_spot():
+                    table.seats.append(list_of_seats[seat_index])
+                    seat_index += 1
+                else:
+                    break
 
-
-        pass
+     
 
     def display(self):
         
         '''The method that display the different tables and there occupants in a nice and readable way'''
-        pass
+        for i, table in enumerate(self.tables, 1):
+            print(f"Table {i}:")
+            for seat in table.seats:
+                print(f"  - {seat.occupant}")
+        else:
+            print(" - No occupants") 
+       
+    
 
     def store(filename :str):
         
@@ -69,4 +95,4 @@ class Openspace(Table):
     def __str__(self) -> str:
 
         "The method returns a human-readable, or informal, string representation of an object"
-        pass
+        
